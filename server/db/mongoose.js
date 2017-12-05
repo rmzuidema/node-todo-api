400d645364392c73b9c2f721be4b7b0ca2f1ad32
rmzuidema <robert.m.zuidema@gmail.com>
2017-12-05 08:56:25 -0500

    Modified connection url to use mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,9 @@ var mongoose = require('mongoose');
 // Use the built in Promise that comes with current JS
 mongoose.Promise = global.Promise;
 // Connect to database
-mongoose.connect('mongodb://localhost:27017/NewTodoApp',{ useMongoClient: true});
+
+mongoose.connect('mongodb://todouser:todouser58@ds129966.mlab.com:29966/newtodoapp');
+//mongoose.connect('mongodb://localhost:27017/newtodoapp',{ useMongoClient: true});
 
 module.exports = {
     mongoose: mongoose
