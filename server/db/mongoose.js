8e20ee8097175761d7374445ee0ff82c1a616749
rmzuidema <robert.m.zuidema@gmail.com>
2017-12-06 08:24:52 -0500

    Updated connection string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,10 +5,10 @@ mongoose.Promise = global.Promise;
 // Connect to database
 
 // Run in Mlab ==> remove comments from line below
-//mongoose.connect('mongodb://todouser:todouser58@ds129966.mlab.com:29966/newtodoapp');
+mongoose.connect('mongodb://todouser:todouser58@ds129966.mlab.com:29966/newtodoapp');
 
 // Run local ==> remove comments from line below
-mongoose.connect('mongodb://localhost:27017/NewTodoApp',{ useMongoClient: true});
+//mongoose.connect('mongodb://localhost:27017/NewTodoApp',{ useMongoClient: true});
 
 module.exports = {
     mongoose: mongoose
