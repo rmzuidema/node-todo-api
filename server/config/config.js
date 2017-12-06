21dcdb92eeb690e85fb54d10bb21075c3f81fab4
rmzuidema <robert.m.zuidema@gmail.com>
2017-12-06 12:19:07 -0500

    Added a config file

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+// Created a local env variable called NODE_ENV and set to local
+
+var env = process.env.NODE_ENV;
+if ( env === 'Local') {
+    process.env.MONGOURI='mongodb://localhost:27017/NewTodoApp';
+} else {
+    process.env.MONGOURI='mongodb://todouser:todouser58@ds129966.mlab.com:29966/newtodoapp';    
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,10 +5,15 @@ mongoose.Promise = global.Promise;
 // Connect to database
 
 // Run in Mlab ==> remove comments from line below
-mongoose.connect('mongodb://todouser:todouser58@ds129966.mlab.com:29966/newtodoapp');
+//mongoose.connect('mongodb://todouser:todouser58@ds129966.mlab.com:29966/newtodoapp',{ useMongoClient: true});
 
 // Run local ==> remove comments from line below
 //mongoose.connect('mongodb://localhost:27017/NewTodoApp',{ useMongoClient: true});
+
+var connectString = process.env.MONGOURI;
+//console.log('connectString ' , connectString);
+
+mongoose.connect(connectString,{ useMongoClient: true});
 
 module.exports = {
     mongoose: mongoose
